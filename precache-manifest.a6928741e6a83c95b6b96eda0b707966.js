c5f9b5bda6efa3bd6f5795af150517448f433968
hongmei <duhongmei@protonmail.com>
2020-07-18 23:27:13 +0800

    Updates

---
+++ precache-manifest.a6928741e6a83c95b6b96eda0b707966.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c79cc001cb7bb4d724284bed7fa883dc",
+    "url": "/react_test_deploy/index.html"
+  },
+  {
+    "revision": "29737883ac17fa3ff1d1",
+    "url": "/react_test_deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "83475338ca73fac00228",
+    "url": "/react_test_deploy/static/js/2.9788b05b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_test_deploy/static/js/2.9788b05b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29737883ac17fa3ff1d1",
+    "url": "/react_test_deploy/static/js/main.7b8ee329.chunk.js"
+  },
+  {
+    "revision": "2dffc50b99c8e3a56ac1",
+    "url": "/react_test_deploy/static/js/runtime-main.f4314e03.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_test_deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
